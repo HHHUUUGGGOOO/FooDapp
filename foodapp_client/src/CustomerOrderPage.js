3b5fac6436903fda98724e86accf16029fa4ff9a
cornliu <cornliu@gmail.com>
2021-06-12 09:59:44 +0800

    'CustomerOrderPagedine'

--- foodapp_client/src/CustomerOrderPage.js
+++ foodapp_client/src/CustomerOrderPage.js
@@ -1,121 +1,133 @@
 import {
-  Box, Button, CircularProgress, Divider, Grid,
-  makeStyles, Paper, Typography, TextField
+    Box, Button, CircularProgress, Divider, Grid,
+    makeStyles, Paper, Typography, TextField
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import Web3 from 'web3'
 const useStyles = makeStyles((theme) => ({
-  customerOrderPaper: {
-  },
-  customerOrderTitle: {
-    padding: theme.spacing(2),
-  },
-  customerOrderMenu: {
-    padding: theme.spacing(2),
-  },
-  customerOrderFooterBox: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  customerOrderTipBox: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  customerOrderTipTextField: {
-    width: '30%',
-  },
-  customerOrderButtonWrapper: {
-    position: 'relative',
-  },
-  customerOrderButtonProgress: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -12,
-    marginLeft: -12,
-  },
+    customerOrderPaper: {
+    },
+    customerOrderTitle: {
+        padding: theme.spacing(2),
+    },
+    customerOrderMenu: {
+        padding: theme.spacing(2),
+    },
+    customerOrderFooterBox: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    customerOrderTipBox: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    customerOrderTipTextField: {
+        width: '30%',
+    },
+    customerOrderButtonWrapper: {
+        position: 'relative',
+    },
+    customerOrderButtonProgress: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
 }))
 
 export default function CustomerOrderPage(props) {
-  const { isLoading, setIsLoading } = props.isLoadingPair;
-  const { web3, accounts, contract } = props.web3States;
-  const orderDetail = props.orderDetail;
-  const orderTime = props.orderTime;
-  const classes = useStyles();
-  const [isTakingOrder, setIsTakingOrder] = useState(false);
-  const [itemsNumber, setItemsNumber] = useState(new Array(orderDetail[5].split('\n').length));
-  const [tipValue, setTipValue] = useState(0);
-  //test
-  const load_order_basic_info_by_orderID = async () => {
-    // setIsLoading(true);
-    // console.log("loading order basic info...");
-    // console.log("loaded.")
-    // setIsLoading(false);
-  }
-  const handleTakeOrder = async () => {
-    // console.log(orderDetail);
-    // setIsTakingOrder(true);
-    // await load_order_basic_info_by_orderID();
-    // setIsTakingOrder(false);
-    const newPostID = 0;
-    console.log(newPostID);
-    console.log(Number(orderDetail[0]));
-    console.log(itemsNumber);
-    console.log(tipValue);
-    await contract.methods.UserSetMyOrderPost(newPostID, Number(orderDetail[0]), itemsNumber, tipValue).send({ from: accounts[0] });
-  }
+    const { isLoading, setIsLoading } = props.isLoadingPair;
+    const { web3, accounts, contract } = props.web3States;
+    const orderDetail = props.orderDetail;
+    const orderTime = props.orderTime;
+    const classes = useStyles();
+    const [isTakingOrder, setIsTakingOrder] = useState(false);
+    const [itemsNumber, setItemsNumber] = useState(new Array(orderDetail[5].split('\n').length));
+    const [tipValue, setTipValue] = useState(0);
+    //test
+    const load_order_basic_info_by_orderID = async () => {
+        // setIsLoading(true);
+        // console.log("loading order basic info...");
+        // console.log("loaded.")
+        // setIsLoading(false);
+    }
+    const handleTakeOrder = async () => {
+        // console.log(orderDetail);
+        // setIsTakingOrder(true);
+        // await load_order_basic_info_by_orderID();
+        // setIsTakingOrder(false);
+        const newPostID = 0;
+        console.log(newPostID);
+        console.log(Number(orderDetail[0]));
+        console.log(itemsNumber);
+        console.log(tipValue);
+        await contract.methods.UserSetMyOrderPost(newPostID, Number(orderDetail[0]), itemsNumber, tipValue).send({ from: accounts[0] });
+    }
 
 
-  return (
-    <Paper className={classes.customerOrderPaper}>
-      <Box className={classes.customerOrderTitle}>
-        <Typography variant="h3" gutterBottom>{orderDetail[2]}</Typography>
-        <Typography variant="subtitle1">{orderDetail[3]}</Typography>
-        <Typography variant="subtitle1">{orderTime}</Typography>
-      </Box>
-      <Divider />
-      <Box className={classes.customerOrderMenu}>
-        {orderDetail[5].split("\n").map((dish, dish_index) => (
-          <Grid container xs={12} spacing={1}>
-            <Grid item xs={12} sm={9}>
-                <Typography>{dish}</Typography>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <TextField
-                onChange={(event) => {
-                  // console.log(event.target.value);
-                  let itemsNumberChange = itemsNumber;
-                  itemsNumberChange[dish_index] = Number(event.target.value);
-                  // console.log(itemsNumberChange);
-                  setItemsNumber(itemsNumberChange);
-                }}
-              />
-            </Grid>
-          </Grid>
-        ))}
-      </Box>
-      <Divider />
-      <Box className={classes.customerOrderFooterBox}>
-        <Box className={classes.customerOrderTipBox}>
-          <Typography variant="h5">Tips: $ </Typography>
-          <TextField
-            className={classes.customerOrderTipTextField}
-            onChange={(event) => { setTipValue(Number(event.target.value)); }}
-          />
-        </Box>
-        <Box className={classes.customerOrderButtonWrapper}>
-          <Button
-            variant="contained"
-            color='primary'
-            onClick={handleTakeOrder}
-            disabled={isTakingOrder}
-          >
-            Take These !
+    return (
+        <Paper className={classes.customerOrderPaper}>
+            <Box className={classes.customerOrderTitle}>
+                <Typography variant="h3" gutterBottom>{orderDetail[2]}</Typography>
+                <Typography variant="subtitle1">{orderDetail[3]}</Typography>
+                <Typography variant="subtitle1">{orderTime}</Typography>
+            </Box>
+            <Divider />
+            <Box className={classes.customerOrderMenu}>
+                {orderDetail[5].split("\n").map((dish, dish_index) => (
+                    <Grid container xs={12} spacing={1}>
+                        <Grid item xs={12} sm={9}>
+                            <Typography>{dish}</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <TextField
+                                onChange={(event) => {
+                                    console.log(Number(event.target.value));
+                                    let itemsNumberChange = itemsNumber;
+                                    if (!isNaN(Number(event.target.value))){
+                                        itemsNumberChange[dish_index] = Number(event.target.value);
+                                    }
+                                    else{
+                                        console.log('qq');
+                                        itemsNumberChange[dish_index] = Number(0);
+                                    }
+                                    console.log(itemsNumberChange);
+                                    setItemsNumber(itemsNumberChange);
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                ))}
+            </Box>
+            <Divider />
+            <Box className={classes.customerOrderFooterBox}>
+                <Box className={classes.customerOrderTipBox}>
+                    <Typography variant="h5">Tips: $ </Typography>
+                    <TextField
+                        className={classes.customerOrderTipTextField}
+                        onChange={(event) => { 
+                            let tips = 0;
+                            if (!isNaN(Number(event.target.value))) {
+                                tips = Number(event.target.value)
+                            }
+                            setTipValue(tips); 
+                        }}
+                    />
+                </Box>
+                <Box className={classes.customerOrderButtonWrapper}>
+                    <Button
+                        variant="contained"
+                        color='primary'
+                        onClick={handleTakeOrder}
+                        disabled={isTakingOrder}
+                    >
+                        Take These !
         </Button>
-          {isTakingOrder && <CircularProgress size={24} className={classes.customerOrderButtonProgress} />}
-        </Box>
-      </Box>
-    </Paper>
-  )
+                    {isTakingOrder && <CircularProgress size={24} className={classes.customerOrderButtonProgress} />}
+                </Box>
+            </Box>
+        </Paper>
+    )
 }
